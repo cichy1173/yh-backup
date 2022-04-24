acbbbfe0359bb55cbad0fe05fcb116f1bd088607
Grzegorz Cichocki <cichy1173@gmail.com>
2022-04-25 00:40:18 +0200

    "small changes"

--- main.sh
+++ main.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# declare drive mount and place variable
+# declare drive mount and place variables
 SSD="/mnt/ssd/"
 DISC="/dev/sda1/"
 
 # checking that $SSD have files
 x=$(ls $SSD | wc -l)
 
-if [ 1 -gt $x  ]
+if [ 1 -gt $x ]
 then
     #mounting ssd
     echo "SSD is NOT mounted"
     mount $DISC $SSD
 fi
 
-$(yunohost backup create --apps)
+$(yunohost backup create)
 
 
 
