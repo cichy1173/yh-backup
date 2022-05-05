d1a3e87d96ba34d39c8c26ed216dc36bd8c67a18
Grzegorz Cichocki <cichy1173@gmail.com>
2022-05-05 18:52:52 +0200

    wallabag2 added

--- main.sh
+++ main.sh
@@ -27,7 +27,7 @@ mkdir $BACKUP_DIRECTORY$CURRENT_DATE
 echo "Creating backup"
 # echo "Creating backup" >> $LOG_FILE
 # backuping only hedgedoc, gitea and droppy apps
-yunohost backup create --apps hedgedoc gitea droppy -o $BACKUP_DIRECTORY$CURRENT_DATE
+yunohost backup create --apps wallabag2 hedgedoc gitea droppy -o $BACKUP_DIRECTORY$CURRENT_DATE
 echo "Backup created"
 # echo "Backup created" >> $LOG_FILE
 
